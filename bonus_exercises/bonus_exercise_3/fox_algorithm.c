a1fa5ff7039be866df987a911c4299eb30778eec
Ermias Habtegabr <ermiasha@kth.se>
2022-06-01 00:27:13 +0200

    bonus exercise 3

--- bonus_exercises/bonus_exercise_3/fox_algorithm.c
+++ bonus_exercises/bonus_exercise_3/fox_algorithm.c
@@ -4,48 +4,119 @@
 #include <math.h>
 #include <mpi.h>
 
+#define N 100
 
-void initialize_matrices(double **matrix, int N)
+typedef struct {
+	int p;
+	MPIComm comm_cart;
+	MPIComm comm_row;
+	MPIComm comm_col;
+	int q;
+	int my_row;
+	int my_col;
+	int my_rank;
+} GRID;
+
+typedef struct {
+	int n_bar;
+} LOCAL_MATRIX;
+
+void initialize_matrices(double **matrix, int n)
 {
 	int i, j;
-	for (i = 0; i < N; i++)
-		for (j = 0; j < N; j++)
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
 			matrix[i][j] = (double)rand() / RAND_MAX;
 }
 
-void free_allocated_matrix(double **matrix, int N)
+void free_allocated_matrix(double **matrix, int n)
 {
 	int i;
-	for (i = 0; i < N; i++)
+	for (i = 0; i < n; i++)
 		free(matrix[i]);
 	free(matrix);
 }
 
-void allocate_matrix(double ***matrix, int N)
+void allocate_matrix(double ***matrix, int n)
 {
 	int i;
-	*matrix = malloc(N * sizeof(double *));
-	for (i = 0; i < N; i++)
-		(*matrix)[i] = malloc(N * sizeof(double));
+	*matrix = malloc(n * sizeof(double *));
+	for (i = 0; i < n; i++)
+		(*matrix)[i] = malloc(n * sizeof(double));
 }
 
+void calculate_local(double **A, double **B, double **C, int n) 
+{
+	int i, j, k;
+	for (i = 0; i < n; i++) 
+		for (j = 0; j < n; j++) 
+			for (k = 0; k < n; k++) 
+				C[i][j] += A[i][j] * B[j][k];
+
+}
+
+void setup_grid(GRID * grid) {
+	int old_rank;
+	int dims[2];
+	int periodic[2];
+	int coords[2];
+	int belonging_coords[2];
+
+	MPI_Comm_size(MPI_COMM_WORLD, &(grid -> p));
+	MPI_Comm_rank(MPI_COMM_WORLD, &old_rank);
+
+	grid -> q = (int) sqrt((double) grid -> p);
+	dims[0] = dims[1] = 1;
+
+	MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periodic, 1, &(grid -> comm_cart));
+	MPI_Comm_rank(grid -> comm_cart, &(grid -> my_rank));
+	MPI_Cart_coords(grid -> comm_cart, grid -> my_rank, 2, coords);
+
+	grid -> my_row = coords[0];
+	grid -> my_col = coords[1];
+
+	belonging_coords[0] = 0; belonging_coords[1] = 1;
+	MPI_Cart_sub(grid -> comm_cart, belonging_coords, &(grid -> comm_row));
+
+	belonging_coords[0] = 1; belonging_coords[1] = 0;
+	MPI_Cart_sub(grid -> comm_cart, belonging_coords, &(grid -> comm_col));
+}
+
+void fox_algorithm(int n, GRID* grid, double** local_A, double** local_B, double** local_C) {
+	double** temp_A;
+	int step;
+	int bcast_root; 
+	int n_bar;
+	int source;
+	int dest;
+	MPI_Status status;
+
+	n_bar = n/grid -> q;
+	//TODO set to zero
+	
+
+	source = (grid -> my_row+ 1) % grid -> q;
+	dest = (grid -> my_row + grid -> q - 1) % grid -> q;
+
+	//TODO local matrix allocate
+
+	for (step = 0; step < grid -> q; step++) {
+		bcast_root = (grid -> my_row+ step) % grid -> q;
+		if (bcast_root == grid -> my_col) {
+			MPI_Bcast(local_A, 1, MPI_FLOAT, bcast_root, grid->comm_row);
+
+		}
+	}
+
+}
 
 int main(int argc, char *argv[])
 {
-	int i, N, rank, size, provided, iter;
+	int size, provided, iter, nr_processes;
+	// current process rank in 
+	int rank, rank_row, rank_col;
+	// matrices  
 	double **A, **B, **C;
-	MPI_Comm new_comm, comm_row, comm_column;
-
-    MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-	N = sqrt(size);
-	if ((N*N) != size) {
-		printf("Number of processes is not a square\n");
-		MPI_Finalize();
-		return EXIT_FAILURE;
-	}
 
 	allocate_matrix(&A, N);
 	allocate_matrix(&B, N);
@@ -54,27 +125,18 @@ int main(int argc, char *argv[])
 	initialize_matrices(A, N);
 	initialize_matrices(B, N);
 
-	const int ndims = 2;
-	const int dims[2] = {N, N};
-	const int periodic[2] = {1, 1};
+	MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE, &provided);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	int coords[2];
-	MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, periodic, 0, &new_comm);
-	MPI_Comm_rank(new_comm, &rank);
-	MPI_Cart_coords(new_comm, rank, 2, coords);
+	printf("number of processes %d\n", size);
 
-	iter = N;
-	int counter = 0;
-
-	for (i = 0; i < iter; i++) {
-
-		if (coords[0] == (coords[1] + counter)) {
-			printf("Proc %d - (%d, %d)\n", rank, coords[0], coords[1]);
-		}
-
-		counter = (counter + 1) >= N ? 0 : counter + 1;
+	int nr_processes = sqrt(size);
+	if (N % nr_processes != 0) {
+		MPI_Finalize();
+		fprintf(stderr, "Square Root of Processes does not divide Number of elements.\n");
+		return 0;
 	}
-
 
 	free_allocated_matrix(A, N);
 	free_allocated_matrix(B, N);
